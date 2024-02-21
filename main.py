0bddc086f321a6d2f3fb86c844f848d415920931
Mattis <mattis-hamann@web.de>
2024-02-21 17:02:47 +0100

    fix: reload image list and fix for empty path

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, request, url_for
 import argparse
 import os
+import time
 
 app = Flask(__name__)
 
+class ImageGetter:
+    def __init__(self, path, reload_time):
+        self._path = path
+        self._time = 0
+        self._reload_time = reload_time
+        self.img_list = []
+
+    def get_image_list(self):
+        if time.time()-self._time > self._reload_time:
+            img_list = []
+            for file in os.listdir(self._path):
+                if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".avif"):
+                    img_list.append(file)
+            self.img_list = img_list
+            self._time = time.time()
+        return self.img_list
+
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
+    img_list = img_getter.get_image_list() 
     if request.method == 'POST':
         if 'Next' in request.form.keys():
             idx = img_list.index(request.form['images'])
@@ -22,7 +41,11 @@ def home_page():
         image_file = url_for("static", filename=img_name)
         return render_template('index.html', img_list = img_list, img_name = img_name, img_data = image_file)
     else:
-        return render_template('index.html', img_list = img_list, img_name=img_list[0])
+        if len(img_list)>0:
+            return render_template('index.html', img_list = img_list, img_name=img_list[0])
+        else:
+            return render_template('index.html', img_list = img_list, img_name="Image path empty")
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,9 +54,8 @@ if __name__ == '__main__':
     parser.add_argument("--external", help="Whether external access is allowed", action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
     app.static_folder = args.path
-    img_list = []
-    for file in os.listdir(args.path):
-        if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".avif"):
-            img_list.append(file)
+    path = args.path
+    img_getter = ImageGetter(path, 30)
+    img_list = img_getter.get_image_list()
     host = '0.0.0.0' if args.external else '127.0.0.1'
     app.run(debug=True, host=host, port=args.port)
